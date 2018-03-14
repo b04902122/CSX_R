e4599da0b04c20694aaf2effdfac48ff72e52c82
b04902122 <lpc2359@hotmail.com>
2018-03-14 21:00:56 +0800

    Update myRcrawler.r

--- week_2/myRcrawler.r
+++ week_2/myRcrawler.r
@@ -1,1 +1,7 @@
+library(httr)
 
+url <- "https://ww8.ikea.com/ext/iplugins/en_US/data/local-stores/data.json"
+res = GET(url)
+res_json = content(res)
+do.call(rbind,res_json)
+View(data.frame(do.call(rbind,res_json)))
